0cacfd15fce418e8d2e2c5929f58107f03e0db64
Minh Nguyen <mnguyen284@gmail.com>
2016-09-03 02:06:04 +0700

    ValidatorExtensions

---
+++ src/PhantomNet.Entities/_EntityTypes/_CodeBasedEntity/CodeBasedEntityValidatorExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PhantomNet.Entities
+{
+    public static class CodeBasedEntityValidatorExtensions
+    {
+        public static async Task ValidateCode<TEntity, TEntityManager, TEntityAccessor>(this IEntityValidator<TEntity, TEntityManager> validator,
+            TEntityManager manager, TEntityAccessor entityAccessor, TEntity entity, List<GenericError> errors,
+            Func<string, GenericError> invalidCode, Func<string, GenericError> duplicateCode)
+            where TEntity: class
+            where TEntityManager : class, ICodeBasedEntityManager<TEntity>
+            where TEntityAccessor : IEntityAccessor<TEntity>,
+                                    ICodeBasedEntityAccessor<TEntity>
+        {
+            var code = entityAccessor.GetCode(entity);
+            if (string.IsNullOrWhiteSpace(code))
+            {
+                errors.Add(invalidCode(code));
+            }
+            else
+            {
+                var owner = await manager.FindByCodeAsync(code);
+                if (owner != null &&
+                    !string.Equals(entityAccessor.GetId(owner), entityAccessor.GetId(entity)))
+                {
+                    errors.Add(duplicateCode(code));
+                }
+            }
+        }
+    }
+}
---
+++ src/PhantomNet.Entities/_EntityTypes/_NameBasedEntity/NameBasedEntityValidatorExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PhantomNet.Entities
+{
+    public static class NameBasedEntityValidatorExtensions
+    {
+        public static async Task ValidateName<TEntity, TEntityManager, TEntityAccessor>(this IEntityValidator<TEntity, TEntityManager> validator,
+            TEntityManager manager, TEntityAccessor entityAccessor, TEntity entity, List<GenericError> errors,
+            Func<string, GenericError> invalidName, Func<string, GenericError> duplicateName)
+            where TEntity: class
+            where TEntityManager : class, INameBasedEntityManager<TEntity>
+            where TEntityAccessor : IEntityAccessor<TEntity>,
+                                    INameBasedEntityAccessor<TEntity>
+        {
+            var name = entityAccessor.GetName(entity);
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                errors.Add(invalidName(name));
+            }
+            else
+            {
+                var owner = await manager.FindByNameAsync(name);
+                if (owner != null &&
+                    !string.Equals(entityAccessor.GetId(owner), entityAccessor.GetId(entity)))
+                {
+                    errors.Add(duplicateName(name));
+                }
+            }
+        }
+    }
+}
