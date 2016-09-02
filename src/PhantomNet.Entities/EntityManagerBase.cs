ab69262a681de09bac8d89faf97855f45777d31c
Minh Nguyen <mnguyen284@gmail.com>
2016-09-02 23:05:09 +0700

    ExplicitLoading

--- src/PhantomNet.Entities/EntityManagerBase.cs
+++ src/PhantomNet.Entities/EntityManagerBase.cs
@@ -864,6 +864,7 @@ namespace PhantomNet.Entities
 
             if (SupportsExplicitLoadingEntity)
             {
+                result.Results = result.Results.ToList().AsQueryable();
                 await ExplicitLoadingEntityStore.ExplicitLoadAsync(result.Results, CancellationToken);
             }
 
--- src/PhantomNet.Entities/_Stores/IExplicitLoadingEntityStore.cs
+++ src/PhantomNet.Entities/_Stores/IExplicitLoadingEntityStore.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Linq;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,7 +8,7 @@ namespace PhantomNet.Entities
     public interface IExplicitLoadingEntityStore<TEntity> : IDisposable
         where TEntity : class
     {
-        Task ExplicitLoadAsync(IQueryable<TEntity> entities, CancellationToken cancellationToken);
+        Task ExplicitLoadAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken);
 
         Task ExplicitLoadAsync(TEntity entity, CancellationToken cancellationToken);
     }
