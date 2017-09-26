c72b8e9df407f6feb66fa24baf0c0fa6a930b25c
Minh Nguyen <mnguyen284@gmail.com>
2017-09-26 22:48:09 +0700

    Build

--- Build.ps1
+++ Build.ps1
@@ -28,6 +28,12 @@ exec { & dotnet restore }
 
 # exec { & dotnet test .\test\PhantomNet.Entities.Tests -c Release }
 
+if ($env:APPVEYOR_REPO_TAG) {
+	exec { & dotnet pack .\src\PhantomNet.Entities.EntityMarkers -c Release -o ..\..\artifacts }
+	exec { & dotnet pack .\src\PhantomNet.Entities -c Release -o ..\..\artifacts }
+	exec { & dotnet pack .\src\extensions\PhantomNet.Entities.EntityFrameworkCore -c Release -o ..\..\..\artifacts }
+	exec { & dotnet pack .\src\extensions\PhantomNet.Entities.Mvc -c Release -o ..\..\..\artifacts }
+} else {
 	$revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 	$revision = "{0:D4}" -f [convert]::ToInt32($revision, 10);
 	$suffix = "beta-" + $revision
@@ -36,3 +42,4 @@ exec { & dotnet restore }
 	exec { & dotnet pack .\src\PhantomNet.Entities -c Release -o ..\..\artifacts --version-suffix=$suffix }
 	exec { & dotnet pack .\src\extensions\PhantomNet.Entities.EntityFrameworkCore -c Release -o ..\..\..\artifacts --version-suffix=$suffix }
 	exec { & dotnet pack .\src\extensions\PhantomNet.Entities.Mvc -c Release -o ..\..\..\artifacts --version-suffix=$suffix }
+}
